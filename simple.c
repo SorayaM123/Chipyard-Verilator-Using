3fefdcf3658e4d59a2d505dc37a06978b2b2e726
SorayaM123 <108527450+SorayaM123@users.noreply.github.com>
2023-06-12 10:12:26 +0200

    Update simple.c

--- simple.c
+++ simple.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+ int my_a, my_b, my_c;
+
+
+int main() {
+   // simple example
+  
+   int my_sum, my_d = 1;
+   my_a = 2;
+   my_b = 3;
+   my_c = my_a+my_b;
+  my_sum = my_c + my_d; 
+   
+
+   return 0;
+}
 
